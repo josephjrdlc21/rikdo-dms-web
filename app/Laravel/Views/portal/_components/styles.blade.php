aa9c77be793dedd82ed2bf059f11351c3d2d7b77
Joseph Dela Cruz Jr. <josephdelacruzjr4@gmail.com>
2024-09-23 14:12:22 +0800

    sidebar adjustment and logos

--- app/Laravel/Views/portal/_components/styles.blade.php
+++ app/Laravel/Views/portal/_components/styles.blade.php
@@ -13,10 +13,7 @@
 <link rel="stylesheet" href="{{asset('assets/libs/select2/dist/css/select2.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/libs/jquery-selectric/selectric.css')}}">
 
-<link rel="stylesheet" href="{{asset('assets/libs/bootstrap-daterangepicker/daterangepicker.css')}}">
 <link rel="stylesheet" href="{{asset('assets/libs/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/libs/select2/dist/css/select2.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/libs/jquery-selectric/selectric.css')}}">
 <link rel="stylesheet" href="{{asset('assets/libs/bootstrap-timepicker/css/bootstrap-timepicker.min.css')}}">
 <link rel="stylesheet" href="{{asset('assets/libs/bootstrap-tagsinput/dist/bootstrap-tagsinput.css')}}">
 
