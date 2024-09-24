7aed1482ed53c592d21ff083ab121a459212f1d5
Joseph Dela Cruz Jr. <josephdelacruzjr4@gmail.com>
2024-09-24 11:20:33 +0800

    add favicon adjustment errors blade

--- app/Laravel/Views/errors/_components/metas.blade.php
+++ app/Laravel/Views/errors/_components/metas.blade.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
 <title>Oops!</title>
+<link rel="shortcut icon" href="{{asset('assets/img/rikdo-logo.png')}}">
